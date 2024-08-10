bf899fd94740c8d067833f1c2f0621499c04e3b8
Victor Sepulveda <victorsepulveda1994@gmail.com>
2024-08-10 04:30:10 -0400

    chore: Creación de las rutas y páginas correspondientes utilzando lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./modules/auth/auth.routes').then(m => m.AUTH_ROUTES)
+  },
+  {
+    path: 'private',
+    loadChildren: () => import('./modules/private/private.routes').then(m => m.PRIVATE_ROUTES)
+  },
+];
