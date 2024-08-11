a21aaed2e771362ca9db125f944bd8f7f2e7d1ac
Victor Sepulveda <victorsepulveda1994@gmail.com>
2024-08-10 21:02:46 -0400

    feat: Iniciar sesión con el usuario y contraseña

--- src/app/modules/auth/pages/login/login.page.ts
+++ src/app/modules/auth/pages/login/login.page.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../../../../core/services/auth.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.page.html',
   styleUrl: './login.page.scss'
 })
 export class LoginPage {
+  protected username: string = '';
+  protected password: string = '';
 
+  private readonly _authService = inject(AuthService);
+  private readonly _router = inject(Router);
+
+  async login() {
+    try {
+      const authResponse = await this._authService.login({ username: this.username, password: this.password })
+      localStorage.setItem('authToken', authResponse.token);
+      localStorage.setItem('user', JSON.stringify(authResponse));
+      this._router.navigate(['private']);
+    } catch (error) {
+      alert('Credenciales inválidas');
+    }
+  }
+  
 }
