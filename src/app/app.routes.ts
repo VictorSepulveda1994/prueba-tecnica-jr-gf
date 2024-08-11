c215ee6586ab89020a4966fe0228629df21f152d
Victor Sepulveda <victorsepulveda1994@gmail.com>
2024-08-10 20:58:11 -0400

    fix: Protección de vistas con guards de ruta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,16 @@
 import { Routes } from '@angular/router';
+import { AuthGuard } from './core/guards/auth.guard';
+import { LoginGuard } from './core/guards/login.guard';
 
 export const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./modules/auth/auth.routes').then(m => m.AUTH_ROUTES)
+    loadChildren: () => import('./modules/auth/auth.routes').then(m => m.AUTH_ROUTES),
+    canActivate: [LoginGuard],
   },
   {
     path: 'private',
-    loadChildren: () => import('./modules/private/private.routes').then(m => m.PRIVATE_ROUTES)
+    loadChildren: () => import('./modules/private/private.routes').then(m => m.PRIVATE_ROUTES),
+    canActivate: [AuthGuard],
   },
 ];
